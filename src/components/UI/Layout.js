00a1928e2193238710a4ad79eda248e80aeafe14
androgitai <andras.kelemen84@gmail.com>
2022-03-24 21:23:59 +0000

    layout,nav component added

--- src/components/UI/Layout.js
+++ src/components/UI/Layout.js
@@ -1,13 +1,13 @@
-import { Fragment } from 'react';
+import classes from './Layout.module.css';
 
 import MainNavigation from './MainNavigation';
 
 const Layout = props => {
   return (
-    <Fragment>
+    <div className={classes.layout}>
       <MainNavigation />
       {props.children}
-    </Fragment>
+    </div>
   );
 };
 
--- src/components/UI/MainNavigation.js
+++ src/components/UI/MainNavigation.js
@@ -1,10 +1,24 @@
 import { Link } from 'react-router-dom';
 
+import classes from './MainNavigation.module.css';
+import logo from '../../assets/logo.svg';
+import moonIcon from '../../assets/icon-moon.svg';
+import avatarPicture from '../../assets/image-avatar.jpg';
+
 const MainNavigation = () => {
   return (
-    <nav>
-      <Link to='/invoices'>Invoices</Link>
-      <Link to='/invoices/id1'>Invoices</Link>
+    <nav className={classes.nav}>
+      <Link to='/'>
+        <img src={logo} alt='Logo' />
+      </Link>
+      <ul>
+        <li>
+          <img src={moonIcon} alt='Color mode button' />
+        </li>
+        <li>
+          <img src={avatarPicture} alt='Avatar' />
+        </li>
+      </ul>
     </nav>
   );
 };
