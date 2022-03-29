d5d695311ab5ec858677d20f056903a4a2915b27
androgitai <andras.kelemen84@gmail.com>
2022-03-29 20:11:15 +0100

    small refactor

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Layout from './components/UI/Layout/Layout';
 import NotFound from './components/UI/Elements/NotFound';
 import InvoiceDetails from './pages/InvoiceDetailsPage';
 import InvoicesPage from './pages/InvoicesPage';
-import InvoiceForm from './components/Invoices/InvoiceForm/InvoiceForm';
+import InvoiceFormModal from './components/UI/Modals/InvoiceFormModal';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<Navigate to='/invoices' />} />
-        <Route path='/form' element={<InvoiceForm />} />
+        <Route path='/form' element={<InvoiceFormModal />} />
         <Route path='/invoices' element={<InvoicesPage />} />
         <Route path='/invoices/:invoiceId' element={<InvoiceDetails />} />
         <Route
