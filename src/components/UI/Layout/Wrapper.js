3b843a43a403a91b16fee04723c3d0ccff987152
androgitai <andras.kelemen84@gmail.com>
2022-03-31 22:46:59 +0100

    finetune and trasitions added

--- src/components/UI/Layout/Wrapper.js
+++ src/components/UI/Layout/Wrapper.js
@@ -1,7 +1,15 @@
 import classes from './Wrapper.module.css';
 
 const Wrapper = props => {
-  return <div className={classes.wrapper}>{props.children}</div>;
+  return (
+    <div
+      className={
+        props.wrapType === 'form' ? classes.wrapperForm : classes.wrapper
+      }
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default Wrapper;
