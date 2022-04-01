91e1f3c63a3af121f693b8b6a69d19c5557405b4
androgitai <andras.kelemen84@gmail.com>
2022-04-01 12:25:01 +0100

    NoInvoice page shown if 0 invoices

--- src/pages/InvoicesPage.js
+++ src/pages/InvoicesPage.js
@@ -1,14 +1,19 @@
+import { useSelector } from 'react-redux';
+
 import Wrapper from '../components/UI/Layout/Wrapper';
 import InvoicesHead from '../components/Invoices/InvoicesHead';
 import InvoicesList from '../components/Invoices/InvoicesList';
 import NoInvoices from '../components/UI/Elements/NoInvoices';
 
 const InvoicesPage = () => {
+  const totalInvoices = useSelector(state => state.invoices.totalInvoices);
+  console.log(totalInvoices);
+
   return (
     <Wrapper>
       <InvoicesHead />
       <InvoicesList />
-      {/* <NoInvoices /> */}
+      {!totalInvoices && <NoInvoices />}
     </Wrapper>
   );
 };
