b4eb8da9dbaa2db5e686e3ed59f1156861921bcf
androgitai <andras.kelemen84@gmail.com>
2022-04-01 23:41:54 +0100

    404 page added

--- src/components/UI/Elements/NotFound.js
+++ src/components/UI/Elements/NotFound.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import classes from './NotFound.module.css';
+import emptySVG from '../../../assets/illustration-empty.svg';
 
 const NotFound = () => {
-  return <h1>404 - Page not found!</h1>;
+  return (
+    <div className={classes.empty}>
+      <img src={emptySVG} alt='No Invoices' />
+      <h1>404 - Page Not Found!</h1>
+      <div>
+        <p>The page you are trying to reach does not exist!</p>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
