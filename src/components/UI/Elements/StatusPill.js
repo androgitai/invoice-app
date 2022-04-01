a9d8b03fc99e6d57293f753f3874862ba74b4bdd
androgitai <andras.kelemen84@gmail.com>
2022-04-01 23:42:16 +0100

    bugfix

--- src/components/UI/Elements/StatusPill.js
+++ src/components/UI/Elements/StatusPill.js
@@ -1,7 +1,7 @@
 import classes from './StatusPill.module.css';
 
 const StatusPill = props => {
-  const status = props.status;
+  const status = props.status.charAt(0).toUpperCase() + props.status.slice(1);
   const pillClasses = `${classes[status]} ${classes.pill}`;
 
   return (
