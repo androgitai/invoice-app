b1863030ce5c819988f9b98c99eaffbbac892a7e
androgitai <andras.kelemen84@gmail.com>
2022-04-01 23:43:45 +0100

    redux invoice slice added

--- src/store/invoices-slice.js
+++ src/store/invoices-slice.js
@@ -4,13 +4,39 @@ import data from '../assets/data.json';
 const invoicesInitialState = {
   invoices: data,
   totalInvoices: data.length,
+  currentInvoice: null,
 };
 
 const invoicesSlice = createSlice({
   name: 'invoices',
   initialState: invoicesInitialState,
   reducers: {
-    getInvoices(state) {},
+    getInvoice(state, action) {
+      const requestedID = action.payload;
+      const currentInvoice = state.invoices.find(
+        item => item.id === requestedID
+      );
+      state.currentInvoice = currentInvoice ? currentInvoice : null;
+      state.totalInvoices = state.invoices.length;
+    },
+    markAsPaid(state, action) {
+      const id = action.payload;
+      console.log(id);
+      const currentInvoiceIndex = state.invoices.findIndex(
+        item => item.id === id
+      );
+      if (currentInvoiceIndex !== -1) {
+        state.invoices[currentInvoiceIndex].status = 'paid';
+        state.currentInvoice.status = 'paid';
+        state.totalInvoices = state.invoices.length;
+      }
+    },
+    deleteInvoice(state, action) {
+      const id = action.payload;
+      state.invoices = state.invoices.filter(item => item.id !== id);
+      state.currentInvoice = null;
+      state.totalInvoices = state.invoices.length;
+    },
   },
 });
 
