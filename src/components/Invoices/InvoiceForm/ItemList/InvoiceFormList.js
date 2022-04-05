ead51233e4554a1891d5051366bb7816c1278eae
androgitai <andras.kelemen84@gmail.com>
2022-04-05 20:50:29 +0100

    reducer moved to cutom hook

--- src/components/Invoices/InvoiceForm/ItemList/InvoiceFormList.js
+++ src/components/Invoices/InvoiceForm/ItemList/InvoiceFormList.js
@@ -1,90 +1,24 @@
-import { Fragment, useReducer } from 'react';
+import { Fragment } from 'react';
 
 import InvoiceFormListItem from './InvoiceFormListItem';
 import Button from '../../../UI/Elements/Button';
 import InvoiceFieldset from '../Layout/InvoiceFieldset';
 import classes from './InvoiceFormList.module.css';
 
-const listItemsReducer = (state, action) => {
-  if (action.type === 'ADD_NEW_ITEM') {
-    return [
-      ...state,
-      {
-        id: state.length,
-        name: '',
-        quantity: 1,
-        price: 0,
-        total: 0,
-      },
-    ];
-  }
-  if (action.type === 'DELETE_ITEM') {
-    const id = action.id;
-    const filteredState = state
-      .filter(item => item.id !== id)
-      .map((item, index) => {
-        return { ...item, id: index };
-      });
-    return filteredState;
-  }
-  if (action.type === 'UPDATE_INPUT') {
-    const { id, key, value } = action;
-    console.log(id, key, value);
-    const newState = [...state];
-    newState.forEach(item => {
-      if (item.id === id) {
-        switch (key) {
-          case 'name':
-            item[key] = value;
-            break;
-          case 'quantity': {
-            item[key] = +value;
-            item.total = item.quantity * item.price;
-            break;
-          }
-          case 'price': {
-            item[key] = +value;
-            item.total = item.quantity * item.price;
-            break;
-          }
-          default:
-            break;
-        }
-      }
-    });
-    return newState;
-  }
-
-  return listItemsReducer;
-};
-
 const InvoiceFormList = props => {
-  const initialState = props.items.map((item, index) => {
-    return {
-      id: index,
-      name: item.name,
-      quantity: item.quantity,
-      price: item.price,
-      total: item.price * item.quantity,
-    };
-  });
-  const [listItemsState, dispatchListItem] = useReducer(
-    listItemsReducer,
-    initialState
-  );
-  console.log(listItemsState);
+  const { items } = props;
 
   const addNewListItemHandler = () => {
-    dispatchListItem({ type: 'ADD_NEW_ITEM' });
+    props.dispatchChange({ type: 'ADD_NEW_ITEM' });
   };
 
   const removeListItemHandler = id => {
-    dispatchListItem({ type: 'DELETE_ITEM', id });
+    props.dispatchChange({ type: 'DELETE_ITEM', id });
   };
 
   const updateListItemField = (id, key, value) => {
     // console.log(id, key, value);
-    dispatchListItem({ type: 'UPDATE_INPUT', id, key, value });
+    props.dispatchChange({ type: 'UPDATE_INPUT', id, key, value });
   };
 
   return (
@@ -94,7 +28,7 @@ const InvoiceFormList = props => {
         legendStyle={classes.header}
         gridType='itemList'
       >
-        {listItemsState.map(item => {
+        {items.map(item => {
           return (
             <InvoiceFormListItem
               key={item.id}
