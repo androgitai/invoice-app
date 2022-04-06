b9607c39fcf7e38794a8c6ba0c9a9fe1c295f094
androgitai <andras.kelemen84@gmail.com>
2022-04-06 18:30:26 +0100

    refactor and cleanup

--- src/store/invoices-slice.js
+++ src/store/invoices-slice.js
@@ -1,61 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import data from '../assets/data.json';
-
-const emptyFormTemplate = {
-  id: '',
-  createdAt: '',
-  paymentDue: '',
-  description: '',
-  paymentTerms: 30,
-  clientName: '',
-  clientEmail: '',
-  status: '',
-  senderAddress: {
-    street: '19 Union Terrace',
-    city: 'London',
-    postCode: 'E1 3EZ',
-    country: 'United Kingdom',
-  },
-  clientAddress: {
-    street: '',
-    city: '',
-    postCode: '',
-    country: '',
-  },
-  items: [
-    {
-      id: 0,
-      name: '',
-      quantity: 0,
-      price: 0,
-      total: 0,
-    },
-  ],
-  total: 0,
-};
-
-const calculateAndFormatDueDate = (date, days) => {
-  let result = new Date(date);
-  result.setDate(result.getDate() + days);
-
-  const newPaymentDueDate = [
-    result.getFullYear(),
-    (result.getMonth() + 1).toString().padStart(2, '0'),
-    result.getDate().toString().padStart(2, '0'),
-  ].join('-');
-  return newPaymentDueDate;
-};
-
-const idGenerator = () => {
-  const DEFAULT_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const getNewChar = () =>
-    DEFAULT_ALPHABET.charAt(
-      Math.floor(Math.random() * DEFAULT_ALPHABET.length)
-    );
-  const getNewNumber = Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
-  const newId = getNewChar() + getNewChar() + getNewNumber;
-  return newId;
-};
+import { emptyFormTemplate, generateInvoice } from '../lib/invoice-utility';
 
 const invoicesInitialState = {
   invoices: data,
@@ -70,17 +15,13 @@ const invoicesSlice = createSlice({
   reducers: {
     getInvoice(state, action) {
       const requestedID = action.payload;
-      const currentInvoice = state.invoices.find(
-        item => item.id === requestedID
-      );
+      const currentInvoice = state.invoices.find(item => item.id === requestedID);
       state.currentInvoice = currentInvoice ? currentInvoice : null;
       state.totalInvoices = state.invoices.length;
     },
     markAsPaid(state, action) {
       const id = action.payload;
-      const currentInvoiceIndex = state.invoices.findIndex(
-        item => item.id === id
-      );
+      const currentInvoiceIndex = state.invoices.findIndex(item => item.id === id);
       if (currentInvoiceIndex !== -1) {
         state.invoices[currentInvoiceIndex].status = 'paid';
         state.currentInvoice.status = 'paid';
@@ -93,61 +34,40 @@ const invoicesSlice = createSlice({
       state.currentInvoice = null;
       state.totalInvoices = state.invoices.length;
     },
-    saveAsDraftInvoice(state, action) {
-      console.log(
-        action.payload.submittedData,
-        action.payload.listItemsState,
-        action.payload.id
+    submittedInvoiceHandler(state, action) {
+      const { id, submittedData, listItemsState, submitType, currentInvoiceStatus } =
+        action.payload;
+
+      const newInvoiceItem = generateInvoice(
+        id,
+        submittedData,
+        listItemsState,
+        submitType,
+        currentInvoiceStatus
       );
-      const { id, submittedData, listItemsState } = action.payload;
 
-      let status = state.currentInvoice.status;
-      if (id === 'new') {
-        const newId = idGenerator();
-        status = 'draft';
-
-        return;
+      if (id === 'new' && submitType === 'draft') {
+        state.invoices.push(newInvoiceItem);
+        console.log('Saved as draft...');
       }
-
-      const grandTotalAmount = listItemsState.reduce(
-        (sum, item) => (sum += item.total),
-        0
-      );
-
-      const newDueDate = calculateAndFormatDueDate(
-        submittedData.createdAt,
-        +submittedData.paymentTerms
-      );
-
-      const invoiceIndex = state.invoices.findIndex(
-        invoice => invoice.id === id
-      );
-      const newInvoiceItem = {
-        id: id,
-        createdAt: submittedData.createdAt,
-        paymentDue: newDueDate,
-        description: submittedData.description,
-        paymentTerms: +submittedData.paymentTerms,
-        clientName: submittedData.clientName,
-        clientEmail: submittedData.clientEmail,
-        status: status,
-        senderAddress: {
-          street: submittedData.senderAddressStreet,
-          city: submittedData.senderAddressCity,
-          postCode: submittedData.senderAddressPostcode,
-          country: submittedData.senderAddressCountry,
-        },
-        clientAddress: {
-          street: submittedData.clientAddressStreet,
-          city: submittedData.clientAddressCity,
-          postCode: submittedData.clientAddressPostcode,
-          country: submittedData.clientAddressCountry,
-        },
-        items: listItemsState,
-        total: grandTotalAmount,
-      };
-      state.invoices[invoiceIndex] = newInvoiceItem;
-      state.currentInvoice = newInvoiceItem;
+      if (id !== 'new' && submitType === 'draft') {
+        const invoiceIndex = state.invoices.findIndex(invoice => invoice.id === id);
+        state.invoices[invoiceIndex] = newInvoiceItem;
+        state.currentInvoice = newInvoiceItem;
+        console.log('Saved as draft...');
+      }
+      if (id === 'new' && submitType === 'send') {
+        console.log('Validating...');
+        state.invoices.push(newInvoiceItem);
+        console.log('Invoice sent...');
+      }
+      if (id !== 'new' && submitType === 'send') {
+        console.log('Validating...');
+        const invoiceIndex = state.invoices.findIndex(invoice => invoice.id === id);
+        state.invoices[invoiceIndex] = newInvoiceItem;
+        state.currentInvoice = newInvoiceItem;
+        console.log('Invoice sent...');
+      }
     },
   },
 });
