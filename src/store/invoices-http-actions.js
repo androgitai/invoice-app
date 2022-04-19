d2c058aa41beece9396b85ea0d94c7f9cd8620c8
androgitai <andras.kelemen84@gmail.com>
2022-04-19 18:15:44 +0100

    fetch one invoice,mark as pad, delete added

--- src/store/invoices-http-actions.js
+++ src/store/invoices-http-actions.js
@@ -1,4 +1,5 @@
 import { invoicesActions } from './invoices-slice';
+import store from './index';
 
 export const fetchAllInvoices = () => {
   return async dispatch => {
@@ -22,7 +23,7 @@ export const fetchAllInvoices = () => {
   };
 };
 
-export const updateInvoiceList = (
+export const updateInvoice = (
   newInvoiceItem,
   id,
   submitType,
@@ -68,4 +69,70 @@ export const updateInvoiceList = (
       console.log(error.message);
     }
   };
+};
+
+export const fetchInvoice = invoiceId => {
+  return async dispatch => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://invoice-app-41f77-default-rtdb.europe-west1.firebasedatabase.app/invoices.json`
+      );
+
+      if (!response) throw new Error('Fetching invoices failed.');
+
+      const data = await response.json();
+      return data;
+    };
+
+    try {
+      const data = await fetchData();
+      dispatch(invoicesActions.setCurrentInvoice({ data, invoiceId }));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+};
+
+export const markInvoiceAsPaid = currentInvoiceIndex => {
+  return async dispatch => {
+    const sendRequest = async () => {
+      const response = await fetch(
+        `https://invoice-app-41f77-default-rtdb.europe-west1.firebasedatabase.app/invoices/${currentInvoiceIndex}/status.json`,
+        {
+          method: 'PUT',
+          body: JSON.stringify('paid'),
+        }
+      );
+
+      if (!response) throw new Error('Something went wrong...');
+    };
+
+    try {
+      await sendRequest();
+      dispatch(invoicesActions.markAsPaid(currentInvoiceIndex));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+};
+
+export const updateInvoiceList = async () => {
+  const sendRequest = async invoices => {
+    const response = await fetch(
+      `https://invoice-app-41f77-default-rtdb.europe-west1.firebasedatabase.app/invoices.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(invoices),
+      }
+    );
+
+    if (!response) throw new Error('Something went wrong...');
+  };
+
+  try {
+    const { invoices } = store.getState().invoices;
+    await sendRequest(invoices);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
