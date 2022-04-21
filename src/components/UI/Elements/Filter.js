a90931414d49d01e283a4e26a2bdaa14277eb98c
androgitai <andras.kelemen84@gmail.com>
2022-04-21 17:44:41 +0100

    bugfix for checked state

--- src/components/UI/Elements/Filter.js
+++ src/components/UI/Elements/Filter.js
@@ -1,9 +1,13 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { invoicesActions } from '../../../store/invoices-slice';
 import Card from '../Layout/Card';
 
 const Filter = () => {
   const dispatch = useDispatch();
+  const filters = useSelector(state => state.invoices.filterBy);
+  const paidChecked = filters.includes('paid');
+  const pendingdChecked = filters.includes('pending');
+  const draftChecked = filters.includes('draft');
 
   const filterCheckedHandler = event => {
     const clickedBox = event.target.id;
@@ -15,19 +19,19 @@ const Filter = () => {
       <ul>
         <li>
           <label htmlFor='paid' onClick={filterCheckedHandler}>
-            <input name='paid' type='checkbox' id='paid' />
+            <input name='paid' defaultChecked={paidChecked} type='checkbox' id='paid' />
             <h4>Paid</h4>
           </label>
         </li>
         <li>
           <label htmlFor='pending' onClick={filterCheckedHandler}>
-            <input name='pending' type='checkbox' id='pending' />
+            <input name='pending' defaultChecked={pendingdChecked} type='checkbox' id='pending' />
             <h4>Pending</h4>
           </label>
         </li>
         <li>
           <label htmlFor='draft' onClick={filterCheckedHandler}>
-            <input name='draft' type='checkbox' id='draft' />
+            <input name='draft' defaultChecked={draftChecked} type='checkbox' id='draft' />
             <h4>Draft</h4>
           </label>
         </li>
