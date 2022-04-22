3e77df822fd41d3ab8f5b0982c66e539b5d16af4
androgitai <andras.kelemen84@gmail.com>
2022-04-22 17:09:33 +0100

    refactored and notifications added

--- src/store/invoices-http-actions.js
+++ src/store/invoices-http-actions.js
@@ -1,99 +1,120 @@
 import { invoicesActions } from './invoices-slice';
+import { uiActions } from './ui-slice';
 
 const URL = 'https://invoice-app-41f77-default-rtdb.europe-west1.firebasedatabase.app';
 
-const invoiceHttp = async (subURL, options = {}) => {
-  const response = await fetch(`${URL}${subURL}`, options);
+const invoiceHttp = async (dispatch, subURL, options = {}) => {
+  dispatch(uiActions.setIsLoading());
+  const httpRequest = async () => {
+    const response = await fetch(`${URL}${subURL}`, options);
 
-  if (!response) throw new Error('Something went wrong...');
-  const data = response.json();
-  return data;
+    if (!response) throw new Error('Something went wrong...');
+    const data = response.json();
+    return data;
+  };
+  try {
+    const data = await httpRequest();
+    dispatch(uiActions.unSetIsLoading());
+    return data;
+  } catch (error) {
+    dispatch(
+      uiActions.showNotification({
+        status: 'error',
+        title: 'Error',
+        message: 'Something went wrong...',
+      })
+    );
+    console.log(error.message);
+  }
 };
 
 export const fetchAllInvoices = () => {
   return async dispatch => {
-    try {
-      const invoices = await invoiceHttp(`/invoices.json`);
-      dispatch(invoicesActions.updateInvoices(invoices));
-    } catch (error) {
-      console.log(error.message);
-    }
+    const invoices = await invoiceHttp(dispatch, `/invoices.json`);
+    dispatch(invoicesActions.updateInvoices(invoices));
   };
 };
 
 export const fetchInvoice = invoiceId => {
   return async dispatch => {
-    try {
-      const invoice = await invoiceHttp(`/invoices/${invoiceId}.json`);
-      dispatch(invoicesActions.setCurrentInvoice({ invoice, invoiceId }));
-    } catch (error) {
-      console.log(error.message);
-    }
+    const invoice = await invoiceHttp(dispatch, `/invoices/${invoiceId}.json`);
+    dispatch(invoicesActions.setCurrentInvoice({ invoice, invoiceId }));
   };
 };
 
 export const deleteInvoice = invoiceId => {
   return async dispatch => {
-    try {
-      await invoiceHttp(`/invoices/${invoiceId}.json`, {
-        method: 'DELETE',
-      });
-      dispatch(invoicesActions.deleteInvoice(invoiceId));
-    } catch (error) {
-      console.log(error.message);
-    }
+    await invoiceHttp(dispatch, `/invoices/${invoiceId}.json`, {
+      method: 'DELETE',
+    });
+    dispatch(invoicesActions.deleteInvoice(invoiceId));
+    dispatch(
+      uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Invoice successfully deleted!',
+      })
+    );
   };
 };
 
 export const updateInvoiceStatus = invoiceId => {
   return async dispatch => {
-    try {
-      await invoiceHttp(`/invoices/${invoiceId}/.json`, {
-        method: 'PATCH',
-        body: JSON.stringify({ status: 'paid' }),
-      });
-      dispatch(invoicesActions.updateInvoiceStatus(invoiceId));
-    } catch (error) {
-      console.log(error.message);
-    }
+    await invoiceHttp(dispatch, `/invoices/${invoiceId}/.json`, {
+      method: 'PATCH',
+      body: JSON.stringify({ status: 'paid' }),
+    });
+    dispatch(invoicesActions.updateInvoiceStatus(invoiceId));
+    dispatch(
+      uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Invoice successfully marked as Paid!',
+      })
+    );
   };
 };
 
 export const updateInvoice = (updatedInvoice, invoiceId) => {
   return async dispatch => {
-    try {
-      await invoiceHttp(`/invoices/${invoiceId}.json`, {
-        method: 'PUT',
-        body: JSON.stringify(updatedInvoice),
-      });
-      dispatch(
-        invoicesActions.submittedFormUpdateInvoiceHandler({
-          updatedInvoice,
-          invoiceId,
-        })
-      );
-    } catch (error) {
-      console.log(error.message);
-    }
+    await invoiceHttp(dispatch, `/invoices/${invoiceId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(updatedInvoice),
+    });
+    dispatch(
+      invoicesActions.submittedFormUpdateInvoiceHandler({
+        updatedInvoice,
+        invoiceId,
+      })
+    );
+    dispatch(
+      uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Invoice successfully updated!',
+      })
+    );
   };
 };
 export const sendNewInvoice = newInvoice => {
   return async dispatch => {
-    try {
-      const { name: newId } = await invoiceHttp(`/invoices.json`, {
-        method: 'POST',
-        body: JSON.stringify(newInvoice),
-      });
-      console.log(newId);
-      dispatch(
-        invoicesActions.submittedFormNewInvoiceHandler({
-          newInvoice,
-          newId,
-        })
-      );
-    } catch (error) {
-      console.log(error.message);
-    }
+    const { name: newId } = await invoiceHttp(dispatch, `/invoices.json`, {
+      method: 'POST',
+      body: JSON.stringify(newInvoice),
+    });
+    dispatch(
+      invoicesActions.submittedFormNewInvoiceHandler({
+        newInvoice,
+        newId,
+      })
+    );
+    dispatch(
+      uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'New invoice seccessfully saved!',
+      })
+    );
   };
 };
 
