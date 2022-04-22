68bc656b37a4d6fda2f2fddc90bcad3ba8769b17
androgitai <andras.kelemen84@gmail.com>
2022-04-22 17:09:51 +0100

    notification bubble added

---
+++ src/components/UI/Elements/Notification.js
@@ -0,0 +1,24 @@
+import classes from './Notification.module.css';
+
+const Notification = props => {
+  const { status, title, message } = props.notification;
+  let specialClasses = '';
+
+  if (status === 'error') {
+    specialClasses = classes.error;
+  }
+  if (status === 'success') {
+    specialClasses = classes.success;
+  }
+
+  const cssClasses = `${classes.notification} ${specialClasses}`;
+
+  return (
+    <section className={cssClasses}>
+      <h3>{title}</h3>
+      <p>{message}</p>
+    </section>
+  );
+};
+
+export default Notification;
