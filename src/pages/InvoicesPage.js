e3e249c06751aa9da930065f6cb34ad4daafea5e
androgitai <andras.kelemen84@gmail.com>
2022-04-22 17:11:13 +0100

    cleanup & NoInvoices element  moved

--- src/pages/InvoicesPage.js
+++ src/pages/InvoicesPage.js
@@ -1,18 +1,12 @@
-import { useSelector } from 'react-redux';
-
 import Wrapper from '../components/UI/Layout/Wrapper';
 import InvoicesHead from '../components/Invoices/InvoicesHead';
 import InvoicesList from '../components/Invoices/InvoicesList';
-import NoInvoices from '../components/UI/Elements/NoInvoices';
 
 const InvoicesPage = () => {
-  const totalInvoices = useSelector(state => state.invoices.totalInvoices);
-
   return (
     <Wrapper>
-      <InvoicesHead totalInvoices={totalInvoices} />
+      <InvoicesHead />
       <InvoicesList />
-      {!totalInvoices && <NoInvoices />}
     </Wrapper>
   );
 };
