a2be2f986bc62df8f70b47ff03a5c1a88a4a3fe9
androgitai <andras.kelemen84@gmail.com>
2022-04-22 17:09:13 +0100

    ui slice connected

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import invoicesSlice from './invoices-slice';
+import uiSlice from './ui-slice';
 
 const store = configureStore({
   reducer: {
     invoices: invoicesSlice.reducer,
+    ui: uiSlice.reducer,
   },
 });
 
