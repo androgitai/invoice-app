fb52ee23fcc513a0db197cba6be31aee5b96ee79
androgitai <andras.kelemen84@gmail.com>
2022-04-23 22:44:25 +0100

    auth form login and register added

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,30 +1,61 @@
-import { useState } from 'react';
+import { useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { authActions } from '../../store/auth-slice';
+import { loginUser, registerUser } from '../../store/auth-http-actions';
 
 import classes from './AuthForm.module.css';
 import Button from '../UI/Elements/Button';
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const { isLogin, isLoading } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const switchAuthModeHandler = () => {
-    setIsLogin(prevState => !prevState);
+    dispatch(authActions.toggleIsLogin());
+  };
+
+  const submitHandler = event => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassrord = passwordInputRef.current.value;
+
+    //validation
+
+    if (!isLogin) {
+      dispatch(registerUser(enteredEmail, enteredPassrord));
+      navigate('/auth');
+    }
+    if (isLogin) {
+      dispatch(loginUser(enteredEmail, enteredPassrord));
+    }
   };
 
   return (
-    <section className={classes.auth}>
+    <section className={classes.auth} key={isLogin}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
-          <Button btnType='primary'>{isLogin ? 'Login' : 'Create Account'}</Button>
-          <Button btnType='discard' className={classes.toggle} onClick={switchAuthModeHandler}>
+          <Button type='submit' btnType='primary' disabled={isLoading}>
+            {isLoading ? 'Loading...' : isLogin ? 'Login' : 'Create Account'}
+          </Button>
+          <Button
+            disabled={isLoading}
+            btnType='discard'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
             {isLogin ? 'Create new account' : 'Login with existing account'}
           </Button>
         </div>
