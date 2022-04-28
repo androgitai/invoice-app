67b55e4334ed67d1a377dd7f858e37f75d8e438e
androgitai <andras.kelemen84@gmail.com>
2022-04-28 21:43:59 +0100

    profile slice connected

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import invoicesSlice from './invoices-slice';
 import authSlice from './auth-slice';
 import uiSlice from './ui-slice';
+import profileSlice from './profile-slice';
 
 const store = configureStore({
   reducer: {
     invoices: invoicesSlice.reducer,
     ui: uiSlice.reducer,
     auth: authSlice.reducer,
+    profile: profileSlice.reducer,
   },
 });
 
