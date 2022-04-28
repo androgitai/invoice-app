f5bf663ada58339aa0ee7d320f016304142f266a
androgitai <andras.kelemen84@gmail.com>
2022-04-28 21:43:16 +0100

    added user profile page

--- src/components/Profile/UserProfile.js
+++ src/components/Profile/UserProfile.js
@@ -1,11 +1,29 @@
-import ProfileForm from './ProfileForm';
+import { useSelector } from 'react-redux';
+
+import PasswordForm from './PasswordForm';
 import classes from './UserProfile.module.css';
+import ProfileForm from './ProfileForm';
+import avatarPic from '../../assets/image-avatar.jpg';
+import Card from '../UI/Layout/Card';
+import ProfileConfirmModal from '../UI/Modals/ProfileConfirmModal';
 
 const UserProfile = () => {
+  const showDetailsConfirmModal = useSelector(state => state.ui.showDetailsConfirmModal);
+  const profile = useSelector(state => state.profile);
+
   return (
     <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
+      {showDetailsConfirmModal && <ProfileConfirmModal />}
+      <div className={classes.profileHead}>
+        <img src={avatarPic} alt='Profile Pic' className={classes.profilePic} />
+        <h1 className={classes.profileH1}>{profile.name}</h1>
+      </div>
+      <div className={classes.profileDetails}>
+        <Card cardType='profile'>
+          <ProfileForm profileData={profile} />
+          <PasswordForm />
+        </Card>
+      </div>
     </section>
   );
 };
