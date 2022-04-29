2e50e12e53e121a8f1b538011264e9adccb78017
androgitai <andras.kelemen84@gmail.com>
2022-04-29 16:25:18 +0100

    fluid nav added

--- src/components/UI/Layout/Layout.js
+++ src/components/UI/Layout/Layout.js
@@ -1,9 +1,12 @@
 import classes from './Layout.module.css';
 import MainNavigation from '../MainNavigation';
+import { useSelector } from 'react-redux';
 
 const Layout = props => {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
   return (
-    <main className={classes.layout}>
+    <main className={isLoggedIn ? classes.layoutFluid : classes.layout}>
       <MainNavigation />
       {props.children}
     </main>
