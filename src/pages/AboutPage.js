6e0b5d94451f88c8e5d56848238566bc0f3a69a2
androgitai <andras.kelemen84@gmail.com>
2022-04-30 22:30:24 +0100

    about page added

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+import About from '../components/About/About';
+
+const AboutPage = () => {
+  return (
+    <Fragment>
+      <About />
+    </Fragment>
+  );
+};
+
+export default AboutPage;
