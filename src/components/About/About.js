de7b4d8ff1dffb731c768feb9dae334744a75225
androgitai <andras.kelemen84@gmail.com>
2022-04-30 22:26:39 +0100

    about page initial setup

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import classes from './About.module.css';
+
+const About = () => {
+  return (
+    <section className={classes.aboutPage}>
+      <h1>About our app...</h1>
+      <h2>It's great! ;)</h2>
+    </section>
+  );
+};
+
+export default About;
