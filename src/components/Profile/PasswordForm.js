4e22fe127c659bd9edf4ce70df6ac957086f3b8b
androgitai <andras.kelemen84@gmail.com>
2022-05-04 22:53:07 +0100

    refactored for with modal

--- src/components/Profile/PasswordForm.js
+++ src/components/Profile/PasswordForm.js
@@ -1,33 +1,59 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useRef } from 'react';
 import { changeUserPassword } from '../../store/auth-http-actions';
 
 import classes from './PasswordForm.module.css';
 import Button from '../UI/Elements/Button';
+import PasswordConfirmModal from '../UI/Modals/PasswordConfirmModal';
+import { useState } from 'react';
+import { uiActions } from '../../store/ui-slice';
 
 const PasswordForm = () => {
   const idToken = useSelector(state => state.auth.idToken);
+  const showPasswordDetailsConfirmModal = useSelector(
+    state => state.ui.showPasswordDetailsConfirmModal
+  );
   const dispatch = useDispatch();
-  const newPasswordInputRef = useRef();
-  const confirmNewPasswordInputRef = useRef();
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredConfirmPassword, setEnteredConfirmPassword] = useState('');
+
+  const passwordInputChangeHandler = event => {
+    setEnteredPassword(event.target.value);
+  };
+  const confirmPasswordInputChangeHandler = event => {
+    setEnteredConfirmPassword(event.target.value);
+  };
 
   const passwordChangeSubitHandler = event => {
     event.preventDefault();
-    const newPassword = newPasswordInputRef.current.value;
-    const confirmNewPassword = confirmNewPasswordInputRef.current.value;
-    //validation missing
-    dispatch(changeUserPassword(newPassword, confirmNewPassword));
+
+    if (enteredPassword.length < 6 || !/^[a-zA-Z0-9]*$/.test(enteredPassword)) {
+      console.log(enteredPassword.length);
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message:
+            'Invalid new password, it can only contain numbers and letters and has to at least 6 characters long',
+        })
+      );
+      return;
+    }
+    dispatch(uiActions.showPasswordModal());
   };
 
   return (
     <form className={classes.form} onSubmit={passwordChangeSubitHandler} key={idToken}>
+      {showPasswordDetailsConfirmModal && (
+        <PasswordConfirmModal password={enteredPassword} confirmPassword={enteredConfirmPassword} />
+      )}
       <label htmlFor='new-password'>New Password</label>
       <input
         type='password'
         id='new-password'
         min='6'
         placeholder='New password'
-        ref={newPasswordInputRef}
+        value={enteredPassword}
+        onChange={passwordInputChangeHandler}
       />
       <label htmlFor='confirm-new-password'>Confirm New Password</label>
       <input
@@ -35,7 +61,8 @@ const PasswordForm = () => {
         id='confirm-new-password'
         min='6'
         placeholder='Confirm new password'
-        ref={confirmNewPasswordInputRef}
+        value={enteredConfirmPassword}
+        onChange={confirmPasswordInputChangeHandler}
       />
       <Button btnType='primary' type='submit'>
         Change Password
