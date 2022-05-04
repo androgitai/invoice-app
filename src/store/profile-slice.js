26cc1bff85c5641c731e15b029c88b03310dcdf1
androgitai <andras.kelemen84@gmail.com>
2022-05-04 22:55:48 +0100

    cleanup & adjustments

--- src/store/profile-slice.js
+++ src/store/profile-slice.js
@@ -1,9 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { initialProfileState } from '../lib/profile-utility';
+import { profileTemplate } from '../lib/form-templates';
+
+let initialState = { ...profileTemplate };
 
 const profileSlice = createSlice({
   name: 'profileSlice',
-  initialState: initialProfileState,
+  initialState: initialState,
   reducers: {
     setProfile(state, action) {
       const newProfile = action.payload;
