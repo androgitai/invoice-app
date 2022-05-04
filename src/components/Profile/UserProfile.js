0cdf64ea268be9b2eed4b5927d2b5f58abc7da3f
androgitai <andras.kelemen84@gmail.com>
2022-05-04 22:53:33 +0100

    refactored for modal

--- src/components/Profile/UserProfile.js
+++ src/components/Profile/UserProfile.js
@@ -2,25 +2,22 @@ import { useSelector } from 'react-redux';
 
 import PasswordForm from './PasswordForm';
 import classes from './UserProfile.module.css';
-import ProfileForm from './ProfileForm';
 import Card from '../UI/Layout/Card';
-import ProfileConfirmModal from '../UI/Modals/ProfileConfirmModal';
 import Avatar from '../UI/Elements/Avatar';
+import ProfileForm from './ProfileForm';
 
 const UserProfile = () => {
-  const showDetailsConfirmModal = useSelector(state => state.ui.showDetailsConfirmModal);
   const profile = useSelector(state => state.profile);
 
   return (
     <section className={classes.profile}>
-      {showDetailsConfirmModal && <ProfileConfirmModal />}
       <div className={classes.profileHead}>
         <Avatar />
         <h1 className={classes.profileH1}>{profile.name}</h1>
       </div>
       <div className={classes.profileDetails}>
         <Card cardType='profile'>
-          <ProfileForm profileData={profile} />
+          <ProfileForm />
           <PasswordForm />
         </Card>
       </div>
