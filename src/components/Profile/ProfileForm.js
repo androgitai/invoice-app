d92756879b7dfaa12172a45d67ef4b29c1010f95
androgitai <andras.kelemen84@gmail.com>
2022-05-05 21:39:13 +0100

    rebuild for new useForm hook

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import useForm from '../../hooks/use-form';
 import { uiActions } from '../../store/ui-slice';
-import { profileErrorsTemplate } from '../../lib/form-templates';
+import useForm from '../../hooks/use-form';
 
 import classes from './ProfileForm.module.css';
 import Button from '../UI/Elements/Button';
@@ -14,33 +13,42 @@ const ProfileForm = props => {
   const showProfileDetailsConfirmModal = useSelector(
     state => state.ui.showProfileDetailsConfirmModal
   );
-  const { formState, formErrors, fullFormValidity, validating, dispatchFormChange, setValidating } =
-    useForm(currentProfile, profileErrorsTemplate);
+  console.log(currentProfile);
+  const { formState, formErrors, formValidity, dispatchFormChange, setIsSubmitting } = useForm(
+    currentProfile,
+    { canBeEmpty: true, isInvoice: false }
+  );
 
   const profileFormSubmitHandler = event => {
     event.preventDefault();
-    setValidating(true);
-    if (fullFormValidity.isFormValid) {
+    setIsSubmitting(true);
+    if (formValidity.isFormValid) {
       dispatch(uiActions.showProfileModal());
     }
-    setTimeout(() => setValidating(false), 5000);
+    setTimeout(() => setIsSubmitting(false), 5000);
   };
 
   const onInputChangeHandler = event => {
-    const inputType = event.target.type;
     const inputId = event.target.id;
     const inputValue = event.target.value;
-    console.log(inputType, inputId, inputValue);
-    dispatchFormChange({ type: 'UPDATE_FORM_INPUT', inputId, inputValue, inputType });
+    console.log(inputId, inputValue);
+    dispatchFormChange({ type: 'UPDATE_FORM_INPUT', inputId, inputValue, isInvoice: false });
   };
 
   useEffect(() => {
-    dispatchFormChange({ type: 'UPDATE_FORM_STATE', currentProfile });
+    dispatchFormChange({
+      type: 'UPDATE_FORM_STATE',
+      newFormState: currentProfile,
+      canBeEmpty: true,
+      isInvoice: false,
+    });
+    console.log('updating');
   }, [dispatchFormChange, currentProfile]);
+
   return (
     <form className={classes.profileText} onSubmit={profileFormSubmitHandler} noValidate>
       {showProfileDetailsConfirmModal && <ProfileConfirmModal profileData={formState} />}
-      <label htmlFor='name' id='name' className={formErrors.name.length && classes.error}>
+      <label htmlFor='name' id='name' className={formErrors.name?.length && classes.error}>
         <h3>Name:</h3>
         <p>{formErrors.name?.[0]}</p>
         <input
@@ -51,7 +59,7 @@ const ProfileForm = props => {
           onChange={onInputChangeHandler}
         />
       </label>
-      <label htmlFor='phone' id='phone1' className={formErrors.phone1.length && classes.error}>
+      <label htmlFor='phone' id='phone1' className={formErrors.phone1?.length && classes.error}>
         <h3>Phone:</h3>
         <p>{formErrors.phone1?.[0]}</p>
         <input
@@ -62,7 +70,7 @@ const ProfileForm = props => {
           onChange={onInputChangeHandler}
         />
       </label>
-      <label htmlFor='email' id='email' className={formErrors.email.length && classes.error}>
+      <label htmlFor='email' id='email' className={formErrors.email?.length && classes.error}>
         <h3>Email:</h3>
         <p>{formErrors.email?.[0]}</p>
         <input
@@ -74,7 +82,7 @@ const ProfileForm = props => {
         />
       </label>
       <h3>Default Address:</h3>
-      <label htmlFor='street' id='street' className={formErrors.street.length && classes.error}>
+      <label htmlFor='street' id='street' className={formErrors.street?.length && classes.error}>
         <h4>Street:</h4>
         <p>{formErrors.street?.[0]}</p>
         <input
@@ -85,7 +93,7 @@ const ProfileForm = props => {
           onChange={onInputChangeHandler}
         />
       </label>
-      <label htmlFor='city' id='city' className={formErrors.city.length && classes.error}>
+      <label htmlFor='city' id='city' className={formErrors.city?.length && classes.error}>
         <h4>City:</h4>
         <p>{formErrors.city?.[0]}</p>
         <input
@@ -99,7 +107,7 @@ const ProfileForm = props => {
       <label
         htmlFor='postCode'
         id='postCode'
-        className={formErrors.postCode.length && classes.error}
+        className={formErrors.postCode?.length && classes.error}
       >
         <h4>Postcode:</h4>
         <p>{formErrors.postCode?.[0]}</p>
@@ -111,7 +119,7 @@ const ProfileForm = props => {
           onChange={onInputChangeHandler}
         />
       </label>
-      <label htmlFor='country' id='country' className={formErrors.country.length && classes.error}>
+      <label htmlFor='country' id='country' className={formErrors.country?.length && classes.error}>
         <h4>Country:</h4>
         <p>{formErrors.country?.[0]}</p>
         <input
